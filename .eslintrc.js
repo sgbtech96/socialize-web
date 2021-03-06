1b19e972dc516f2647d3c3b91485cdc0a1d575b2
Siddhant Gandhi <sgbtech96@gmail.com>
2021-03-06 22:26:02 +0530

    build fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,28 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
-  plugins: ['react'],
+  plugins: ["react"],
   rules: {
-    'react/prop-types': 0,
-    'no-unused-vars': 0,
-    'no-undef': 0,
-    'no-empty': 0,
-    'react/jsx-no-duplicate-props': 0,
+    "react/prop-types": 0,
+    "no-unused-vars": 0,
+    "no-undef": 0,
+    "no-empty": 0,
+    "react/jsx-no-duplicate-props": 0,
     curly: 1,
     eqeqeq: 1,
-    'no-console': 'warn',
+    "no-console": "warn",
   },
 };
